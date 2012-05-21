44fef8afbb7d22f20ae2d48268b02f69773e7127
Jeff Browning <jeff@jkbrowning.com>
2012-05-21 00:09:52 -0500

    Added initial tests for achievement model

--- lib/snafu/models/achievement.rb
+++ lib/snafu/models/achievement.rb
@@ -1,7 +1,23 @@
 module Snafu
   module Models
     class Achievement
-      
+      attr_reader :name, :description, :url, :image_60, :image_180
+      alias_method :desc, :description
+      def initialize(options = {})
+        @name = options[:name]
+        @description = options[:description] || options[:desc]
+        @url = options[:url]
+        if options[:image_60].is_a? String
+          @image_60 = GlitchImage.new(url: options[:image_60])
+        elsif options[:image_60].is_a? GlitchImage
+          @image_60 = options[:image_60]
+        end
+        if options[:image_180].is_a? String
+          @image_180 = GlitchImage.new(url: options[:image_180])
+        elsif options[:image_180].is_a? GlitchImage
+          @image_180 = options[:image_180]
+        end
+      end
     end
   end
 end
--- spec/snafu/models/achievement_spec.rb
+++ spec/snafu/models/achievement_spec.rb
@@ -3,7 +3,29 @@ require 'spec_helper'
 module Snafu
   module Models
     describe Snafu::Models::Achievement do
-      it "should populate values if given an options hash"
+      before(:each) do
+        @valid_options = {
+          name: "a name",
+          desc: "a description",
+          url: "http://example.com",
+          image_60: "http://example.com/60.png",
+          image_180: "http://example.com/180.png"
+        }
+        @achievement = Achievement.new @valid_options
+      end
+
+      it "should populate values if given an options hash" do
+        @achievement.name.should eql(@valid_options[:name])
+        @achievement.description.should eql(@valid_options[:desc])
+        @achievement.url.should eql(@valid_options[:url])
+        @achievement.image_60.url.should eql(@valid_options[:image_60])
+        @achievement.image_180.url.should eql(@valid_options[:image_180])
+      end
+      
+      it "should alias #description as #desc" do
+        @achievement.desc.should eql(@achievement.description)
+      end
+
     end
   end
 end
